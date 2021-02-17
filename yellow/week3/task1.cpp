bc409f10e82ec205226879695ebf5730ac4f7275
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-02-17 03:28:56 +0300

    Add files via upload

---
+++ yellow/week3/task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "sum_reverse_sort.h"
+#include <algorithm>
+
+int Sum(int x, int y){
+    return x+y;
+}
+string Reverse(string s){
+    reverse(s.begin(),s.end());
+    return s;
+}
+void Sort(vector<int>& nums){
+    sort(begin(nums),end(nums));
+}
