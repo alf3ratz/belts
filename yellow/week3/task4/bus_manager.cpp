83568a278611447706a7b3468b64feeb8991b864
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-02-17 04:46:56 +0300

    Add files via upload

---
+++ yellow/week3/task4/bus_manager.cpp
@@ -0,0 +1,36 @@
+
+#include "bus_manager.h"
+
+using  namespace std;
+
+
+void BusManager::AddBus(const string& bus, const vector<string>& stops) {
+    buses_for_stops.insert(make_pair(bus, stops));
+    for (const auto& stop : stops) {
+        stops_for_buses[stop].push_back(bus);
+    }
+}
+
+BusesForStopResponse BusManager::GetBusesForStop(const string& stop) const {
+    if (stops_for_buses.count(stop) == 0) {
+        return BusesForStopResponse{vector<string>()};
+    } else {
+        return BusesForStopResponse{stops_for_buses.at(stop)};
+    }
+}
+
+StopsForBusResponse BusManager::GetStopsForBus(const string& bus) const {
+    vector<pair<string, vector<string>>> result;
+
+    if (buses_for_stops.count(bus) > 0) {
+        for (const auto& stop : buses_for_stops.at(bus)) {
+            result.push_back(make_pair(stop, stops_for_buses.at(stop)));
+        }
+    }
+    return StopsForBusResponse{bus, result};
+}
+
+AllBusesResponse BusManager::GetAllBuses() const {
+    return AllBusesResponse{buses_for_stops};
+}
+
